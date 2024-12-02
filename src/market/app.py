58ffff6e0af63377bc272112a0e07587de234462
leonardo@cyborg <leomanne2000@gmail.com>
2024-12-02 18:12:20 +0100

    added thread timer for auction + auto-remove of gatcha after end of auction from user collection

--- src/market/app.py
+++ src/market/app.py
@@ -1,11 +1,13 @@
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 import os
+import threading
 from flask import Flask, request, make_response, jsonify
 from pymongo import MongoClient
 import bson.json_util as json_util
 from pymongo.errors import ServerSelectionTimeoutError
 import requests
 from auth_utils import role_required, get_userID_from_jwt
+
 
 USER_URL = os.getenv('USER_URL')
 
@@ -25,7 +27,8 @@ def add_auction():
     except Exception as e:
         return make_response(jsonify({"error": str(e)}), 401)
     
-    
+    #TODO: check if gatcha exists in user collection
+    #TODO: erase gatcha from user collection
     
     auction = {
         "Auction_ID": Auctions.count_documents({}) + 1, # TODO: maybe add hash here (?)
@@ -38,7 +41,27 @@ def add_auction():
         "end_time": datetime.now() + timedelta(minutes=10)
     }
     try:
+
+        jwt_token = request.headers.get('Authorization')
+        headers = {'Authorization': jwt_token}
+        # Erase from user collection the gatcha that is being auctioned
+        response = requests.post(
+            USER_URL + "/remove_gatcha",
+            json={"gatcha_ID": auction["Gatcha_ID"]},headers=headers
+        )
+        if response.status_code != 200:
+            return make_response(jsonify({"error": "Failed to remove gatcha from user"}), 500)
+
         Auctions.insert_one(auction)
+
+
+        # Start a thread to delete the auction after the specified end_time
+        delay_seconds = (auction["end_time"] - auction["creation_time"].total_seconds())
+        thread = threading.Thread(target=delete_auction_after_delay, args=(auction["Auction_ID"], delay_seconds,auction["Gatcha_ID"]))
+        thread.daemon = True  # Ensures the thread ends if the main app stops
+        thread.start()
+
+
         return make_response(jsonify({"message": "Auction added successfully"}), 201)
     except Exception as e:
         return make_response(jsonify({"error": str(e)}), 500)
@@ -51,7 +74,7 @@ def delete_auction():
     if not auction_id:
         return make_response(jsonify({"error": "Auction_ID is required"}), 400)
     try:
-        auction = Auctions.find_one({"Auction_ID": bid["Auction_ID"]})
+        auction = Auctions.find_one({"Auction_ID": auction_id}) ## correzione di auction_id id
         if not auction:
             return make_response(jsonify({"error": "Auction not found"}), 404)
         
@@ -73,6 +96,34 @@ def delete_auction():
             return make_response(jsonify({"error": "Auction not found"}), 404)
     except Exception as e:
         return make_response(jsonify({"error": str(e)}), 500)
+    
+
+#   Thread function to delete an auction after a delay.
+def delete_auction_after_delay(auction_id, delay_seconds,gatcha_id):
+    
+    time.sleep(delay_seconds)
+    try:
+        auction = Auctions.find_one({"Auction_ID": auction_id}) ## correzione di auction_id id
+        if not auction:
+            return make_response(jsonify({"error": "Auction not found"}), 404)
+        
+        if auction["Winner_ID"]:
+            winner = auction["Winner_ID"]
+            response = requests.post(
+                USER_URL + "/add_gatcha",
+                json={"username": winner, "gatcha_ID": gatcha_id}
+            )
+        if response.status_code != 200:
+                return make_response(jsonify({"error": "Failed to add gatcha to user"}), 500)
+        
+        result = Auctions.delete_one({"Auction_ID": auction_id})
+        if result.deleted_count == 1:
+            return make_response(jsonify({"message": "Auction ended successfully"}), 200)
+        else:
+            return make_response(jsonify({"error": "Auction not found"}), 404)
+    except Exception as e:
+        return make_response(jsonify({"error": str(e)}), 500)
+
 
 @app.route('/bid', methods=['POST'])
 def bid():

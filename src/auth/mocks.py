e69086bd6564269797abe617eb21fffbcbb50f6f
Emanuele B. <27818313+Emahhh@users.noreply.github.com>
2024-12-06 11:14:50 +0100

    definite mocks

--- src/auth/mocks.py
+++ src/auth/mocks.py
@@ -8,7 +8,7 @@ import logging
 MOCKING HTTP REQUESTS
 
 FUNZIONAMENTO:
-- Vai all'interno delle funzioni handle_get_request e handle_post_request
+- Vai all'interno delle funzione mock_request()
 - Definisci le risposte di default alle richieste GET e POST che vuoi simulare
 - Chiamando start_mocking_http_requests(), ogni richiesta HTTP verrà intercettata, e verrà fornita una risposta in base a quanto definito sotto, e non verrà effettuata una vera richiesta HTTP
 """
@@ -18,39 +18,46 @@ FUNZIONAMENTO:
 logger = logging.getLogger(__name__)
 
 
+# INSERIRE QUI LE RISPOSTE DI DEFAULT PER LE RICHIESTE
+# questa funzione verrà chiamata al posto di requests.get(), requests.post(), ecc., ritornando la risposta definita qui
+def mock_request(method, url, *args, **kwargs):
+    logger.info(f"Intercepted {method} request to URL: {url}")
+    
+    if url.endswith("/init-user"): # Esempio: se la richiesta è a /init-user
+        return MockResponse({"message": "User initialized successfully"}, 201) # alla richiesta verrà risposto con questo JSON e codice di stato 201
+    
+    if url.endswith("/user/delete_user"):
+        return MockResponse({"message": "User deleted successfully"}, 200)
+    
+    # According to the method, call the appropriate function to handle the request
+    if method == 'POST':
+        json_body = kwargs.get('json')
+        logger.info(f"Handling POST request to URL: {url} with body: {json_body}")
+        logger.warning("Returning 404 Not Found for POST request")
+        return MockResponse({"error": "Not Found"}, 404)
+    
+    logger.warning("Returning 404 Not Found for " + method + " request " + url)
+    return MockResponse({"error": "Not Found"}, 404)
+
+
+
+
+
+
+
 def start_mocking_http_requests():
     """
-    call this function to start mocking http requests:
-    every requests.get, requests.post, etc. will be intercepted and handled by the functions below
+    Call this function to start mocking HTTP requests:
+    every requests.get, requests.post, etc. will be intercepted and handled by the functions below.
     """
     
-    logger.info("Starting to mock HTTP requests using the" + start_mocking_http_requests.__name__ + " function")
-
-    # This function will be called every time a request is made with the requests library
-    def mock_request(method, url, *args, **kwargs):
-        logger.info(f"Intercepted {method} request to URL: {url}")
-        return MockResponse({"message": "User initialized successfully"}, 201)
-        
-        # according to the method, call the appropriate function to handle the request
-        if method == 'POST':
-            return handle_post_request(url, kwargs.get('json'))
-        return MockResponse({"error": "Not Found"}, 404)
-
-
-    # LE RISPOSTE ALLE RICHIESTE POST VANNO QUI
-    def handle_post_request(url, json_body):
-        logger.info(f"Handling POST request to URL: {url} with body: {json_body}")
-        
-        if url.endswith("/init-user"):
-            return MockResponse({"message": "User initialized successfully"}, 201)
-        
-        if url.endswith("/user/delete_user"):
-            return MockResponse({"message": "User deleted successfully"}, 200)
-        
-        logger.warning("Returning 404 Not Found for POST request")
+    logger.info("Starting to mock HTTP requests using the " + start_mocking_http_requests.__name__ + " function")
     
-    patcher = patch('requests.request', mock_request)
-    patcher.start()
+    methods_to_patch = ['get', 'post', 'put', 'delete', 'patch', 'head', 'options']
+    patchers = [patch(f'requests.{method}', lambda url, **kwargs: mock_request(method.upper(), url, **kwargs)) for method in methods_to_patch]
+    
+    for patcher in patchers:
+        patcher.start()
 
 
 
@@ -76,6 +83,7 @@ def no_op_decorator(*required_roles):
             return f(*args, **kwargs)
         return decorated_function
     return decorator
+
 
 
 
